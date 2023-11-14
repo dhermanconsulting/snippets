0abe71bf04a3a04ee5a35641ed5a55557cb5ba7c
Dan Herman <48517048+dhermanconsulting@users.noreply.github.com>
2023-11-14 10:35:48 +0000

    Change param + add example

--- decrypt-netscaler-password/Readme.md
+++ decrypt-netscaler-password/Readme.md
@@ -1,3 +1,10 @@
 This is a partial PowerShell implementation of the Python code provided here:
 
 https://dozer.nz/posts/citrix-decrypt/
+
+Example:
+
+./decrypt.ps1 -InputString "b65f2142d01fe706083173b064c04cfc6b81ab2417d39d63d2b3216176d0e638b89cbca0f1c4294db56b66668f94ff0f"
+
+Expected Output:
+
--- decrypt-netscaler-password/decrypt.ps1
+++ decrypt-netscaler-password/decrypt.ps1
@@ -1,6 +1,7 @@
 param(
 
-    [string] $InputString = "b65f2142d01fe706083173b064c04cfc6b81ab2417d39d63d2b3216176d0e638b89cbca0f1c4294db56b66668f94ff0f",
+    [Parameter(Mandatory)]
+    [string] $InputString,
     [ValidateSet("ENCMTHD_3", "ENCMTHD_2", "ENCMTHD_1")]
     [string] $Mode = "ENCMTHD_3"
 
@@ -60,8 +61,6 @@ function Convert-HexToByteArray {
 [byte[]]  $aesKey = Convert-HexToByteArray "351CBE38F041320F22D990AD8365889C7DE2FCCCAE5A1A8707E21E4ADCCD4AD9" # Built-In NetScaler Key
 [byte[]] $rc4Key = Convert-HexToByteArray "2286da6ca015bcd9b7259753c2a5fbc2" # Built-In NetScaler Key
 [byte[]] $cipherText = Convert-HexToByteArray $InputString
-
-# $cipherText = [Convert]::FromBase64String($InputString)
 
 if ($mode -eq "ENCMTHD_3" -or $mode -eq "ENCMTHD_2") {
     $cipher = [AESCipher]::new($aesKey)
